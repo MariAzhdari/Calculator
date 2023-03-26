0a029fd9af7c205b9fb429753a488da0ff514e9d
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-26 23:55:09 +0100

    add index.html

--- Calculator.js
+++ Calculator.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
 });
+
 app.listen(3000, () => {
   console.log(`Example app listening on port `);
 });
