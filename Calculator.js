10d113d487e22d56c4a9facb71d7c5863749b04f
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-27 13:52:31 +0100

    done first step

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-  res.send("Thank you for posting");
+
+let num1 =req.body.num1;
+let num2 = req.body.num2;
+let result =num1+ num2;
+
+  res.send(`The result of the calculation is  ${result} `);
 });
 
 app.listen(3000, () => {
