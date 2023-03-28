32ecd57255063bb6578814b428d8577204642e9a
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-28 22:09:34 +0100

    bmi done

--- Calculator.js
+++ Calculator.js
@@ -18,6 +18,24 @@ let result =num1+ num2;
   res.send(`The result of the calculation is  ${result} `);
 });
 
+//BMI 
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmiCalculator", (req, res) => {
+  let w = parseFloat(req.body.weight);
+  let h = parseFloat(req.body.hight);
+  let bmi = w /(Math.pow(h,2))
+
+  res.send(`The result of the  bmi calculation ${bmi}
+  `);
+});
+
+
+
+
+
+
 app.listen(3000, () => {
   console.log(`Example app listening on port `);
 });
